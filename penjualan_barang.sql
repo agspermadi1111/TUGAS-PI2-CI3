e0d7fc020925dd3fb7d9aa6a8e8c809b4765e7c6
Agus Permadi <agsperr@gmail.com>
2021-07-28 22:57:26 +0700

    COMMIT YUK

---
+++ penjualan_barang.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Jul 2021 pada 17.31
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `barang_id` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `harga_barang` int(15) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`barang_id`, `nama_barang`, `harga_barang`, `stok`, `keterangan`) VALUES
+(16, 'Kecap pedas ABC', 20000, 222, 'cbcmb'),
+(17, 'Kecap pedas ', 30000, 3633, '3bdbvdmn');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `pelanggan_id` int(11) NOT NULL,
+  `nama_pelanggan` varchar(50) NOT NULL,
+  `no_tlp` int(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`pelanggan_id`, `nama_pelanggan`, `no_tlp`, `alamat`) VALUES
+(11, 'budi anduk', 883838383, 'sjsjs'),
+(12, 'cecep', 2147483647, 'sjsjks'),
+(13, 'zaenudin', 48484999, 'sjsjs'),
+(14, 'tredx', 2147483647, 'ghhfhjgv');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `penjualan_id` int(11) NOT NULL,
+  `tgl_penjualan` varchar(50) NOT NULL,
+  `pelanggan_id` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `total` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`penjualan_id`, `tgl_penjualan`, `pelanggan_id`, `keterangan`, `total`) VALUES
+(1627485346, '21-07-28', 11, 'gkhh', NULL),
+(1627485464, '21-07-28', 13, 'jkjk', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_detail`
+--
+
+CREATE TABLE `penjualan_detail` (
+  `penjualan_detail_id` int(11) NOT NULL,
+  `penjualan_id` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `harga_total` int(20) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan_detail`
+--
+
+INSERT INTO `penjualan_detail` (`penjualan_detail_id`, `penjualan_id`, `barang_id`, `harga_total`, `jumlah`, `diskon`) VALUES
+(23, 1627469245, 16, 200000, 10, 0),
+(24, 1627469245, 17, 60000, 2, 0),
+(25, 1627469246, 16, 240000, 12, 0),
+(26, 1627483236, 16, 0, 0, 0),
+(27, 1627477052, 17, 84000, 4, 30),
+(28, 1627485346, 16, 294400, 32, 54),
+(29, 1627485346, 17, 1350000, 45, 0),
+(30, 1627485464, 16, 1080000, 54, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barang_id`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`pelanggan_id`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`penjualan_id`);
+
+--
+-- Indeks untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
+  ADD PRIMARY KEY (`penjualan_detail_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `pelanggan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `penjualan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1627485465;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
+  MODIFY `penjualan_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
